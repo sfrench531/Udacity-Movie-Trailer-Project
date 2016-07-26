b69c2fba95a70c1ef637b22ee5d7a198eaf20a61
Sarah <s.french531@gmail.com>
2016-07-25 17:28:34 -0700

    Add documentation to Movie default constructor

--- media.py
+++ media.py
@@ -13,6 +13,10 @@ class Movie():
 
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
 
+
+    """The default Movie constructor requires movie_title, movie_storyline, poster_image
+        (a url link to the poster image), and trailer_youtube (a url link to the YouTube movie trailer).""" 
+
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
